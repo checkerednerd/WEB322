e870c825e888404616ce753336842f86cac365ae
checkerednerd <ronnyvince@gmail.com>
2018-09-18 08:10:44 -0400

    yeah

--- week02/Ex1/input.js
+++ week02/Ex1/input.js
@@ -13,12 +13,12 @@ function onHttpStart() {
 
 //set up a 'route' to listen on the default url (/)
 app.get("/", function (req, res) {
-    res.send("Hello World<br /><a href='/about'>Go to the page</a>");
+    res.send("Hello World<br /><a href='/about'>Go to the page</a>"); //notice it's in html
 });
 
 //set up another route to listen on /about
 app.get("/about", function (req, res) {
-    res.send("<h3>About</h3>");
+    res.send("<h3>About</h3>"); //notice it's in html
 });
 
 //set up http server to listen on HTTP_PORT
